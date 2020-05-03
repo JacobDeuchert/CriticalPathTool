55c81e408704b5a993c7f8181f4a65d4ab682970
Mona Lisa <jacobdeuchert@web.de>
2020-05-03 11:46:50 +0200

    Changed App name

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/NetzplanApp'),
+      dir: require('path').join(__dirname, '../coverage/CriticalPathTool'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
